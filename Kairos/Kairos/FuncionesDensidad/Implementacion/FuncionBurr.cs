ac9e9e65a1459b721b891abeb3d9894fca594d84
yamilaBacchini <yamicqq@hotmail.com>
2018-09-10 16:43:17 -0300

    stringFDP y stringInversa

--- Kairos/Kairos/FuncionesDensidad/Implementacion/FuncionBurr.cs
+++ Kairos/Kairos/FuncionesDensidad/Implementacion/FuncionBurr.cs
@@ -11,9 +11,13 @@ namespace Kairos.FuncionesDensidad.Implementacion
 {
     class FuncionBurr : FuncionDensidadProbabilidad, IFuncionRepresentable
     {
-        public string StringFDP => throw new NotImplementedException();
+        private readonly string A = "";
+        private readonly string B = "";
+        private readonly string G = "";
+        private readonly string K = "";
+        public string StringFDP => string.Format("({0} * {3} * ((x - {2}) / {1}) ^ ({0} - 1)) / ({1} * (1 + ((x - {2}) / {1}) ^ {0}) ^ ({3} + 1))", A, B, G, K);
 
-        public string StringInversa => throw new NotImplementedException();
+        public string StringInversa => string.Format("x = {1} * (((1 - R) ^ (-1 / {3}) - 1) ^ (1 / {0})) + {2}", A, B, G, K);
 
         public FuncionBurr(double[] eventos) : base(eventos)
         {
