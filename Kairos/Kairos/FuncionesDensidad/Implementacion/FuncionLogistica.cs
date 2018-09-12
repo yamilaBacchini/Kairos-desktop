32f8ff0b01ba021e828110c12ec1771dad1375f7
yamilaBacchini <yamicqq@hotmail.com>
2018-09-12 13:37:18 -0300

    Ajusta logistica

--- Kairos/Kairos/FuncionesDensidad/Implementacion/FuncionLogistica.cs
+++ Kairos/Kairos/FuncionesDensidad/Implementacion/FuncionLogistica.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Accord.Statistics.Analysis;
+using Accord.Statistics.Distributions.Univariate;
 using Kairos.Entidades;
 using Kairos.Modelo;
 
@@ -14,22 +15,24 @@ namespace Kairos.FuncionesDensidad.Implementacion
         private readonly string MU = "";
         private readonly string S = "";
 
-        public string StringFDP => string.Format("(e^(({0}-x)/{1}))/({1}*(1+e^(({0}-x)/{1}))^2)",MU,S);
+        public string StringFDP => string.Format("(e^(({0}-x)/{1}))/({1}*(1+e^(({0}-x)/{1}))^2)", MU, S);
 
-        public string StringInversa =>string.Format("f(R) = {0}-{1}*ln(1/R-1)", MU,S);
+        public string StringInversa => string.Format("f(R) = {0}-{1}*ln(1/R-1)", MU, S);
 
         public FuncionLogistica(double[] eventos) : base(eventos)
         {
             try
             {
-                 var distribucionContinua= new Accord.Statistics.Distributions.Univariate.LogisticDistribution();
-                distribucionContinua.Fit(eventos);
-                 //this.MU = distribucionContinua.Location.ToString("0.0000");
-                 //this.S = DistribucionContinua.Scale.ToString("0.0000");
-                 Resultado = new ResultadoAjuste(StringFDP, StringInversa, distribucionContinua.StandardDeviation, distribucionContinua.Mean, distribucionContinua.Variance, this);
-                 
+                double media = eventos.Average();
+                int n = eventos.Count();
+                double sigma = eventos.Sum(x => Math.Pow(x - media, 2)) / n;
+                var distribucionContinua = new LogisticDistribution(media, sigma);
+                this.MU = distribucionContinua.Location.ToString("0.0000");
+                this.S = distribucionContinua.Scale.ToString("0.0000");
+                Resultado = new ResultadoAjuste(StringFDP, StringInversa, distribucionContinua.StandardDeviation, distribucionContinua.Mean, distribucionContinua.Variance, this);
+
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 Resultado = null;
             }
