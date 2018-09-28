17247587e3d4f8a8e003dbd8b321cd076db93061
yamilaBacchini <yamilaBacchini@users.noreply.github.com>
2018-09-28 11:36:27 -0300

    seteo de parametros shape y scale en weibull (#59)

--- Kairos/Kairos/FuncionesDensidad/Implementacion/FuncionWeibull.cs
+++ Kairos/Kairos/FuncionesDensidad/Implementacion/FuncionWeibull.cs
@@ -11,25 +11,41 @@ namespace Kairos.FuncionesDensidad.Implementacion
 {
     class FuncionWeibull : FuncionDensidadProbabilidad, IFuncionRepresentable
     {
-        private readonly double shape = 0.5;
-        private readonly double scale = 0.5;
+        private readonly double shape = 0;
+        private readonly double scale = 0;
 
-        public string StringFDP => "No implementado aun";
+        public string StringFDP => string.Format("(alfa/beta)*(x/beta)^(alfa-1)*e^(-(x/beta)^alfa)", shape, scale);
 
-        public string StringInversa => "No implementado aun";
+        public string StringInversa => string.Format("f(R)= beta*(-ln(-R+1))^(1/alfa)", shape, scale);
 
         public FuncionWeibull(double[] eventos) : base(eventos)
         {
             try
             {
+                eventos = eventos.Where(x => x > 0).OrderBy(x => x).ToArray();
+                for (int i = 0; i < eventos.Length - 1; i++)
+                {
+                    shape += Math.Log(Math.Log(1 / (1 - Estimador(i + 2, eventos.Length)))) - Math.Log(Math.Log(1 / (1 - Estimador(i + 1, eventos.Length))));
+                }
+                shape /= Math.Log(eventos[eventos.Length - 1]) - Math.Log(eventos[0]);
+                for (int i = 0; i < eventos.Length; i++)
+                {
+                    scale += (Math.Log(1 - Estimador(i + 1, eventos.Length)) / eventos[i]);
+                }
+                scale *= (-1 / (double)eventos.Length);
                 DistribucionContinua = new WeibullDistribution(shape, scale);
-                DistribucionContinua.Fit(eventos);
                 Resultado = new ResultadoAjuste(StringFDP, StringInversa, DistribucionContinua.StandardDeviation, DistribucionContinua.Mean, DistribucionContinua.Variance, this);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 Resultado = null;
             }
+        }
+
+        private double Estimador(int i, int n)
+        {
+            //return i / (n + 1);
+            return (i - 0.3) / (n + 0.4);
         }
     }
 }
