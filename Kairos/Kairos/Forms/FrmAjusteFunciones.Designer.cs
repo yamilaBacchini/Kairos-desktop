7740b90a9a88c4b9613fe5bbe6328c2dede32d47
yamilaBacchini <yamila.bacchini@gmail.com>
2018-11-10 16:18:43 -0300

    detalle estetico en el grafico de la funcion inversa

--- Kairos/Kairos/Forms/FrmAjusteFunciones.Designer.cs
+++ Kairos/Kairos/Forms/FrmAjusteFunciones.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAjusteFunciones));
             this.pnlEventos = new System.Windows.Forms.Panel();
             this.chrtInversa = new System.Windows.Forms.DataVisualization.Charting.Chart();
@@ -100,10 +100,12 @@
             // 
             // chrtInversa
             // 
-            chartArea3.Name = "ChartArea2";
-            this.chrtInversa.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend2";
-            this.chrtInversa.Legends.Add(legend3);
+            this.chrtInversa.BackHatchStyle = System.Windows.Forms.DataVisualization.Charting.ChartHatchStyle.Cross;
+            chartArea1.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.Name = "ChartArea2";
+            this.chrtInversa.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend2";
+            this.chrtInversa.Legends.Add(legend1);
             this.chrtInversa.Location = new System.Drawing.Point(7, 266);
             this.chrtInversa.Name = "chrtInversa";
             this.chrtInversa.Size = new System.Drawing.Size(355, 205);
@@ -112,16 +114,16 @@
             // 
             // chrtFuncion
             // 
-            chartArea4.Name = "ChartArea1";
-            this.chrtFuncion.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.chrtFuncion.Legends.Add(legend4);
+            chartArea2.Name = "ChartArea1";
+            this.chrtFuncion.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chrtFuncion.Legends.Add(legend2);
             this.chrtFuncion.Location = new System.Drawing.Point(7, 32);
             this.chrtFuncion.Name = "chrtFuncion";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chrtFuncion.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chrtFuncion.Series.Add(series1);
             this.chrtFuncion.Size = new System.Drawing.Size(355, 205);
             this.chrtFuncion.TabIndex = 26;
             this.chrtFuncion.Text = "chart1";
